3f54ccd34a4a8f47acd4de6648de02cf8993eb0d
Matata <matias.agf@gmail.com>
2021-05-09 15:53:00 -0300

    Actualizacion package y docs

--- src/herlpers/getGifs.js
+++ src/herlpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async (category) => {
-    const resp = await fetch(`http://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=vwwqJRf0C02iHfXE5STsRszg7A0XJmIG`)
+    const resp = await fetch(`https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=vwwqJRf0C02iHfXE5STsRszg7A0XJmIG`)
     const {data} = await resp.json()
 
     const gifs = data.map(img => {
